20b51aaf90c34254672330f698cda5984bd4ca84
alzaburetz <michael322blazeit@gmail.com>
2019-02-18 23:39:10 +0300

    Pre-pre-pre-alpha with mocking data

---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/shirou/gopsutil/process"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"regexp"
+	"time"
+)
+
+var working = []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"}
+var weekend = []string{"Saturday", "Sunday"}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatalf("%+v", err.Error())
+	} else {
+		log.Println("SUCCESS")
+	}
+}
+
+func TheDay(days []string, day string) bool {
+	result := false
+	for i := 0; i < len(days); i++ {
+		if days[i] == day {
+			result = true
+			break
+		}
+	}
+	return result
+}
+
+func Closable(res *Restriction, nm []*process.Process) error {
+	result := false
+	var err error
+	var i int
+	for i = 0; i < len(nm); i++ {
+		name, _ := nm[i].Name()
+		matched, _ := regexp.Match(name, []byte(res.App))
+		if matched && res.Rule == "Close" {
+			if TheDay(working, time.Now().Weekday().String()) && res.Time == "working" {
+				result = true
+				break
+			}
+			if TheDay(weekend, time.Now().Weekday().String()) && res.Time == "weekend" {
+				result = true
+				break
+			}
+		}
+
+	}
+	if result {
+		err = nm[i].Kill()
+		if err != nil {
+			return err
+		} else {
+			log.Println("Successfully killed " + res.App)
+		}
+
+	}
+	return err
+}
+
+func Openable(res *Restriction, nm []*process.Process) error {
+	var i int
+	var err error
+	var name string
+	for i = 0; i < len(nm); i++ {
+		name, err = nm[i].Name()
+		if res.App == name {
+			break
+		}
+	}
+
+	if i == len(nm) && res.Rule == "Open" {
+		if TheDay(weekend, time.Now().Weekday().String()) && res.Time == "weekend" || TheDay(working, time.Now().Weekday().String()) && res.Time == "working" {
+			go exec.Command(res.Exec).Run()
+
+		}
+	}
+
+	return err
+
+}
+
+func main() {
+	//var logg string
+	//var lo, _ = os.Create("logs/log "+time.Now().String()+".txt")
+	//defer lo.Close()
+	//w := bufio.NewWriter(lo)
+	for {
+		procs, _ := process.Processes()
+		byteVal, _ := ioutil.ReadFile("restrictions.json")
+		var r Restrict
+		json.Unmarshal(byteVal, &r)
+
+		for j := 0; j < len(r.Restrictions); j++ {
+			if time.Now().Hour() > r.Restrictions[j].Hourfrom && time.Now().Hour() < r.Restrictions[j].Hourto {
+				err := Closable(&r.Restrictions[j], procs)
+				if err != nil {
+					log.Printf("Error closing restricted app: %+v", err)
+					break
+				}
+				err = Openable(&r.Restrictions[j], procs)
+				if err != nil {
+					log.Printf("Error opening restricted app: %+v", err)
+					break
+				}
+			}
+		}
+
+		time.Sleep(time.Second)
+	}
+
+}
+
+type Restrict struct {
+	Restrictions []Restriction `json:"restrictions"`
+}
+
+type Restriction struct {
+	App      string `json:"app"`
+	Windows  int    `json:"windows"`
+	Rule     string `json:"rule"`
+	Time     string `json:"time"`
+	Hourfrom int    `json:"hourfrom"`
+	Hourto   int    `json:"hourto"`
+	Exec     string `json:"executable"`
+}
